f475c7a3a75ca03ef8562d12cbd0c773a1a03e8f
Reza Amuzadeh <amuzadeh@vasl.ir>
2019-08-04 15:16:19 +0330

    Update Min SDK To 16 - Update ExoPlayer Version To 2.10.3 - Update Template HLS LINK

--- app/src/main/java/com/potyvideo/andexoplayer/MainActivity.java
+++ app/src/main/java/com/potyvideo/andexoplayer/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String TEST_URL_MP4 = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4";
 
-    private String TEST_URL_HLS = "http://185.23.131.61/hls/jomhori_2.m3u8";
+    private String TEST_URL_HLS = "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8";
 
     private String TEST_URL_MP3 = "https://host2.rj-mw1.com/media/podcast/mp3-192/Tehranto-41.mp3";
 
